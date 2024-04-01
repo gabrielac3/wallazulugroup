4ed5489ac00f5f232aea2d595cb62ae663e80e5c
gabrielac3 <gabycr.3.2.gcr@gmail.com>
2024-03-31 19:45:24 -0500

    cambio de escudos2

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,19 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
-  currentImage:string = 'https://raw.githubusercontent.com/gabrielac3/im-genes/main/wallazulu/ESCUDO-GLOBAL.png';
+export class LayoutComponent implements OnInit {
 
-  currentImageWallazullu: string = 'https://raw.githubusercontent.com/gabrielac3/im-genes/main/wallazulu/ESCUDO-GLOBAL.png';
-  currentImageGeitsu: string = 'https://raw.githubusercontent.com/gabrielac3/im-genes/main/wallazulu/GEITSU.png';
-  currentImageWallaosint: string = 'https://raw.githubusercontent.com/gabrielac3/im-genes/main/wallazulu/WALLAOSINT.png';
+  currentImageWallazullu: string = 'https://raw.githubusercontent.com/gabrielac3/im-genes/main/wallazulu/ESCUDO-GLOBAL2.png';
+  currentImageGeitsu: string = 'https://raw.githubusercontent.com/gabrielac3/im-genes/main/wallazulu/GEITSU2.png';
+  currentImageWallaosint: string = 'https://raw.githubusercontent.com/gabrielac3/im-genes/main/wallazulu/WALLAOSINT2.png';
+  currentImage!: string;
 
-    changeImage(imageUrl: string) {
-      this.currentImage = imageUrl;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+
+    const ruta = this.router.url;
+    this.setCurrentImage(ruta)
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const urlActual = this.router.url;
+        this.setCurrentImage(urlActual);
+      });
+  }
+
+  setCurrentImage(route: string) {
+   
+    switch (route) {
+      case '/home/wallazulu':
+        this.currentImage = this.currentImageWallazullu;
+        break;
+      case '/home/geitsu':
+        this.currentImage = this.currentImageGeitsu;
+        break;
+      case '/home/wallaosint':
+        this.currentImage = this.currentImageWallaosint;
+        break;
+      default:
+        this.currentImage = this.currentImageWallazullu;
+        break;
     }
+  }
 
 }
